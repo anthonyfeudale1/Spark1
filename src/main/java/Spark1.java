970d72ae38255743fa3b693c33194ed9e8a9e433
Randalthor95 <Randalthor95@gmail.com>
2020-02-10 18:00:36 -0500

    Working!

--- src/main/java/Spark1.java
+++ src/main/java/Spark1.java
@@ -14,26 +14,26 @@ public class Spark1 {
     public static void main(String[] args) {
         String linkPath = args[0];
         String titlesPath = args[1];
+        String query = args[2];
+        String outputPath = args[3];
         JavaSparkContext sc = new JavaSparkContext("yarn", "Spark1",
                 "$SPARK_HOME", new String[]{"target/Spark1-1.0.jar"});
 
 
-        JavaPairRDD<Integer, ArrayList<Integer>> links = getFormattedLinks(linkPath, sc).cache();
-        JavaPairRDD<Integer, String> titles = getFormattedTitles(titlesPath, sc).cache();
-        JavaPairRDD<Integer, ArrayList<Integer>> isLinkedToo = getIsLinkedToo(links, sc).cache();
-        isLinkedToo.saveAsTextFile("/output/LinkedToo");
+        JavaPairRDD<Integer, ArrayList<Integer>> links = getFormattedLinks(linkPath, sc);
+        JavaPairRDD<Integer, String> titles = getFormattedTitles(titlesPath, sc);
+        JavaPairRDD<Integer, ArrayList<Integer>> isLinkedToo = getIsLinkedToo(links, sc);
 
-        String query = "a";
-//        while (!query.equals("QUIT")) {
-//            System.out.println("Please enter a search query: ");
-//            Scanner in = new Scanner(System.in);
-//            query = in.nextLine();
 
-        JavaPairRDD<Integer, String> rootSet = getRootSet(query, titles, sc);
-        rootSet.saveAsTextFile("/output/RootSet");
-        JavaRDD<Integer> baseSet = getBaseSet(rootSet, links, sc);
-        baseSet.saveAsTextFile("/output/BaseSet");
-        computeAuthAndHubScores(baseSet, links, isLinkedToo, titles, sc);
+        JavaPairRDD<Integer, String> rootSet = getRootSet(query, titles);
+        if (rootSet.isEmpty()) {
+            System.err.println("The search term '" + query + "' was not found in our pages.");
+            return;
+        }
+        rootSet.saveAsTextFile(outputPath + "/RootSet");
+        JavaRDD<Integer> baseSet = getBaseSet(rootSet, links);
+        baseSet.saveAsTextFile(outputPath + "/BaseSet");
+        computeAndSaveAuthAndHubScores(baseSet, links, isLinkedToo, titles, outputPath);
     }
 
 
@@ -100,7 +100,7 @@ public class Spark1 {
     }
 
 
-    private static JavaPairRDD<Integer, String> getRootSet(String query, JavaPairRDD<Integer, String> titles, JavaSparkContext sc) {
+    private static JavaPairRDD<Integer, String> getRootSet(String query, JavaPairRDD<Integer, String> titles) {
 
         Function<Tuple2<Integer, String>, Boolean> filter = k -> k._2.contains(query);
         return titles.filter(filter);
@@ -108,7 +108,7 @@ public class Spark1 {
 
     }
 
-    private static JavaRDD<Integer> getBaseSet(JavaPairRDD<Integer, String> rootSet, JavaPairRDD<Integer, ArrayList<Integer>> links, JavaSparkContext sc) {
+    private static JavaRDD<Integer> getBaseSet(JavaPairRDD<Integer, String> rootSet, JavaPairRDD<Integer, ArrayList<Integer>> links) {
         JavaRDD<Integer> rootSetNums = rootSet.map(
                 new Function<Tuple2<Integer, String>, Integer>() {
                     public Integer call(Tuple2<Integer, String> entry) {
@@ -143,7 +143,7 @@ public class Spark1 {
         return rootSetNums.union(rootSetPointsTo).union(linksToRootSet).distinct();
     }
 
-    private static void computeAuthAndHubScores(JavaRDD<Integer> baseSet, JavaPairRDD<Integer, ArrayList<Integer>> links, JavaPairRDD<Integer, ArrayList<Integer>> isLinkedToo, JavaPairRDD<Integer, String> titles, JavaSparkContext sc) {
+    private static void computeAndSaveAuthAndHubScores(JavaRDD<Integer> baseSet, JavaPairRDD<Integer, ArrayList<Integer>> links, JavaPairRDD<Integer, ArrayList<Integer>> isLinkedToo, JavaPairRDD<Integer, String> titles, String outputPath) {
 
         JavaPairRDD<Integer, Double> auths = baseSet.mapToPair(
                 new PairFunction<Integer, Integer, Double>() {
@@ -153,33 +153,89 @@ public class Spark1 {
                 }
         );
         JavaPairRDD<Integer, Double> hubs = auths;
+        auths = filterAuths(auths, isLinkedToo);
+        hubs = filterHubs(hubs, links);
 
-        //auths = iterateAuths(hubs, isLinkedToo);
-        hubs = iterateHubs(auths, links);
-        hubs.saveAsTextFile("/output/Temp");
+        for (int i = 0; i < 25; ++i) {
+            auths = iterateAuths(hubs);
+            hubs = iterateHubs(auths);
+        }
+        JavaPairRDD<String, Double> sortedAuths = sortScores(auths.join(titles));
+        JavaPairRDD<String, Double> sortedHubs = sortScores(hubs.join(titles));
+        sortedAuths.saveAsTextFile(outputPath + "/AuthorityScores");
+        sortedHubs.saveAsTextFile(outputPath + "/HubScores");
+
 
     }
 
-//    private static JavaPairRDD<Integer, Double> iterateAuths(JavaPairRDD<Integer, Double> hubs, JavaPairRDD<Integer, ArrayList<Integer>> isLinkedToo) {
-//    }
-
-    private static JavaPairRDD<Integer, Double> iterateHubs(JavaPairRDD<Integer, Double> auths, JavaPairRDD<Integer, ArrayList<Integer>> links) {
-        JavaPairRDD<Integer, Double> scores = links.flatMapToPair(
+    private static JavaPairRDD<Integer, Double> filterAuths(JavaPairRDD<Integer, Double> auths, JavaPairRDD<Integer, ArrayList<Integer>> isLinkedToo) {
+        return auths.join(isLinkedToo.flatMapToPair(
                 new PairFlatMapFunction<Tuple2<Integer, ArrayList<Integer>>, Integer, Integer>() {
                     public Iterator<Tuple2<Integer, Integer>> call(Tuple2<Integer, ArrayList<Integer>> entry) {
                         ArrayList<Tuple2<Integer, Integer>> results = new ArrayList<Tuple2<Integer, Integer>>();
+                        if (entry._2.size() == 0) {
+                            results.add(new Tuple2<Integer, Integer>(entry._1, 0));
+                        }
                         for (int i : entry._2()) {
                             results.add(new Tuple2<Integer, Integer>(i, entry._1));
                         }
                         return results.iterator();
                     }
                 }
-        ).join(auths).mapToPair(
-                new PairFunction<Tuple2<Integer, Tuple2<Integer, Double>>, Integer, Double>() {
-                    public Tuple2<Integer, Double> call(Tuple2<Integer, Tuple2<Integer, Double>> entry) {
-                        return new Tuple2<Integer, Double>(entry._2._1, entry._2._2);
+        )).mapToPair(
+                new PairFunction<Tuple2<Integer, Tuple2<Double, Integer>>, Integer, Double>() {
+                    public Tuple2<Integer, Double> call(Tuple2<Integer, Tuple2<Double, Integer>> entry) {
+                        if (entry._2._2 == 0) {
+                            return new Tuple2<Integer, Double>(entry._1, 0.0);
+                        }
+                        return new Tuple2<Integer, Double>(entry._2._2, entry._2._1);
                     }
-                }).reduceByKey(
+                });
+    }
+
+    private static JavaPairRDD<Integer, Double> filterHubs(JavaPairRDD<Integer, Double> hubs, JavaPairRDD<Integer, ArrayList<Integer>> links) {
+        return hubs.join(links.flatMapToPair(
+                new PairFlatMapFunction<Tuple2<Integer, ArrayList<Integer>>, Integer, Integer>() {
+                    public Iterator<Tuple2<Integer, Integer>> call(Tuple2<Integer, ArrayList<Integer>> entry) {
+                        ArrayList<Tuple2<Integer, Integer>> results = new ArrayList<Tuple2<Integer, Integer>>();
+                        if (entry._2.size() == 0) {
+                            results.add(new Tuple2<Integer, Integer>(entry._1, 0));
+                        }
+                        for (int i : entry._2()) {
+                            results.add(new Tuple2<Integer, Integer>(i, entry._1));
+                        }
+                        return results.iterator();
+                    }
+                }
+        )).mapToPair(
+                new PairFunction<Tuple2<Integer, Tuple2<Double, Integer>>, Integer, Double>() {
+                    public Tuple2<Integer, Double> call(Tuple2<Integer, Tuple2<Double, Integer>> entry) {
+                        if (entry._2._2 == 0) {
+                            return new Tuple2<Integer, Double>(entry._1, 0.0);
+                        }
+                        return new Tuple2<Integer, Double>(entry._2._2, entry._2._1);
+                    }
+                });
+    }
+
+    private static JavaPairRDD<String, Double> sortScores(JavaPairRDD<Integer, Tuple2<Double, String>> scores) {
+        return scores.mapToPair(
+                new PairFunction<Tuple2<Integer, Tuple2<Double, String>>, Double, String>() {
+                    public Tuple2<Double, String> call(Tuple2<Integer, Tuple2<Double, String>> entry) {
+                        return new Tuple2<Double, String>(entry._2._1, entry._2._2);
+                    }
+                }
+        ).sortByKey(false).mapToPair(
+                new PairFunction<Tuple2<Double, String>, String, Double>() {
+                    public Tuple2<String, Double> call(Tuple2<Double, String> entry) {
+                        return new Tuple2<String, Double>(entry._2, entry._1);
+                    }
+                });
+    }
+
+    private static JavaPairRDD<Integer, Double> iterateAuths(JavaPairRDD<Integer, Double> hubs) {
+
+        JavaPairRDD<Integer, Double> scores = hubs.reduceByKey(
                 new Function2<Double, Double, Double>() {
                     @Override
                     public Double call(Double aDouble, Double aDouble2) throws Exception {
@@ -188,6 +244,19 @@ public class Spark1 {
                 }
         );
 
+        return normalize(scores);
+    }
+
+    private static JavaPairRDD<Integer, Double> iterateHubs(JavaPairRDD<Integer, Double> auths) {
+
+        JavaPairRDD<Integer, Double> scores = auths.reduceByKey(
+                new Function2<Double, Double, Double>() {
+                    @Override
+                    public Double call(Double aDouble, Double aDouble2) throws Exception {
+                        return aDouble + aDouble2;
+                    }
+                }
+        );
         return normalize(scores);
 
     }
